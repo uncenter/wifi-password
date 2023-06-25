3b24392e62ff13e9d03b257a1c05cf26aa6d2437
uncenter <47499684+uncenter@users.noreply.github.com>
2023-06-24 21:29:20 -0400

    feat(install): add reinstall and uninstall options, silence curl

--- install.sh
+++ install.sh
@@ -7,11 +7,20 @@ fi
 
 if [ -f "/usr/local/bin/wifi-password" ]; then
     echo "wifi-password is already installed."
-    exit 1
+    read -r -n 1 -p "Do you want to [r]einstall, [u]ninstall, or [a]bort?" REPLY
+    echo
+    if [[ $REPLY =~ ^[Rr]$ ]]; then
+        sudo rm /usr/local/bin/wifi-password
+    elif [[ $REPLY =~ ^[Uu]$ ]]; then
+        sudo rm /usr/local/bin/wifi-password
+        exit 0
+    else
+        exit 0
+    fi
 fi
 
-curl -o wifi-password.sh https://raw.githubusercontent.com/uncenter/wifi-password/master/wifi-password.sh
+curl --silent --show-error --output wifi-password.sh https://raw.githubusercontent.com/uncenter/wifi-password/master/wifi-password.sh
 
 chmod +x wifi-password.sh
 
-sudo mv wifi-password.sh /usr/local/bin/wifi-password && echo "Installation complete. You can now use wifi-password!" || echo "Installation failed."
+sudo mv wifi-password.sh /usr/local/bin/wifi-password && echo "Installation complete. Run \`wifi-password --help\` for usage. " || echo "Installation failed."
