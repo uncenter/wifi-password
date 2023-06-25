5f1d795c9a66f6a539777dd9778ecf4b3ac805f7
uncenter <47499684+uncenter@users.noreply.github.com>
2023-06-24 21:36:59 -0400

    feat: add space at end of reinstallation prompt, prevent overwriting of existing files

--- install.sh
+++ install.sh
@@ -7,20 +7,28 @@ fi
 
 if [ -f "/usr/local/bin/wifi-password" ]; then
     echo "wifi-password is already installed."
-    read -r -n 1 -p "Do you want to [r]einstall, [u]ninstall, or [a]bort?" REPLY
+    read -r -n 1 -p "Do you want to [r]einstall, [u]ninstall, or [a]bort? " REPLY
     echo
     if [[ $REPLY =~ ^[Rr]$ ]]; then
         sudo rm /usr/local/bin/wifi-password
+        echo "wifi-password uninstalled."
     elif [[ $REPLY =~ ^[Uu]$ ]]; then
         sudo rm /usr/local/bin/wifi-password
+        echo "wifi-password uninstalled."
         exit 0
     else
         exit 0
     fi
 fi
 
+if [ -f "./wifi-password.sh" ]; then
+    echo "A file named wifi-password.sh already exists in the current directory."
+    echo "Please remove or rename it before installing."
+    exit 1
+fi
+
 curl --silent --show-error --output wifi-password.sh https://raw.githubusercontent.com/uncenter/wifi-password/master/wifi-password.sh
 
 chmod +x wifi-password.sh
 
-sudo mv wifi-password.sh /usr/local/bin/wifi-password && echo "Installation complete. Run \`wifi-password --help\` for usage. " || echo "Installation failed."
+sudo mv wifi-password.sh /usr/local/bin/wifi-password && echo "wifi-password installed. Run \`wifi-password --help\` for usage. " || echo "Installation failed."
